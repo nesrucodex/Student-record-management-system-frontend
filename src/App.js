6c4c06bc1be05cf808292abdf45b3d5a80f52dfe
Nesredin Getahun <nesrucodex01@gmail.com>
2024-01-15 21:10:05 +0300

    Student record managment system frontend semi-final

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./app.css";
+import Application from "./studnent result management system/Application";
+
+function App() {
+  return (
+    <div className="app">
+      <Application />
+    </div>
+  );
+}
+
+export default App;
